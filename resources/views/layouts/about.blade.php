e15a2adb4cd1ce3adf0ea3c8ce6a6d744eb6ca73
zwan the marvelous <azwangenshin@gmail.com>
2026-02-15 22:02:36 +0800

    i made an about and contact us pages.

---
+++ resources/views/layouts/about.blade.php
@@ -0,0 +1,20 @@
+@extends('layouts.blog')
+ 
+@section('content')
+<main class="relative min-h-screen pt-40 pb-16">
+
+    <div class="container mx-auto relative z-10 px-6">
+
+        <!-- Blog Article Section -->
+        <section class="w-full bg-white/70 backdrop-blur-lg shadow-xl rounded-3xl p-12 lg:p-20">
+            <h1 class="text-2xl font-bold mb-4">About us</h1>
+            <p class="mt-4 text-lg text-gray-600">Welcome to our blog — a space dedicated to hobbies and the things that make life more interesting.
+
+We share simple, relatable posts about different hobbies, from creative interests to everyday passions. Whether you’re here to discover something new or improve at what you already enjoy, this is a place for inspiration and real experiences.
+
+We believe hobbies aren’t just ways to pass time — they’re what keep life fun and meaningful.
+
+Thanks for being here.</p>
+        </section>
+    </main>
+@endsection
