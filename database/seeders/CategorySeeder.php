04216e5e6536ec1a69480712ee5981c7a8a86b51
zwan the marvelous <azwangenshin@gmail.com>
2026-02-15 22:24:11 +0800

    Seeder database

---
+++ database/seeders/CategorySeeder.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class CategorySeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $categories = [
+            ['name' => 'Technology', 'created_at' => now(), 'updated_at' => now()],
+            ['name' => 'Travel', 'created_at' => now(), 'updated_at' => now()],
+            ['name' => 'Food', 'created_at' => now(), 'updated_at' => now()],
+            ['name' => 'Lifestyle', 'created_at' => now(), 'updated_at' => now()],
+            ['name' => 'Health', 'created_at' => now(), 'updated_at' => now()],
+        ];
+
+        DB::table('categories')->insert($categories);
+    }
+}
