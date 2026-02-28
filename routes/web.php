8ce22f10f697a1254cb8e57ef8f400f9683f2b19
zwan the marvelous <azwangenshin@gmail.com>
2026-03-01 04:53:07 +0800

    Added routes for /categories and /posts

--- routes/web.php
+++ routes/web.php
@@ -6,9 +6,24 @@ use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Facades\DB;
 use App\Http\Controllers\PostController;
 use App\Http\Controllers\SigninController;
+use App\Http\Controllers\CategoryController;
+use App\Models\Post;
+use App\Models\Category;
  
 
 Route::get('/', [HomeController::class, 'index'])->middleware('auth')->name('home');
+
+Route::get('/categories', [CategoryController::class, 'index'])->name('categories');
+Route::post('/categories', [CategoryController::class, 'store'])->name('categories.store');
+Route::patch('/categories/{category}', [CategoryController::class, 'update'])->name('categories.update');
+Route::delete('/categories/{category}', [CategoryController::class, 'destroy'])->name('categories.destroy');
+Route::patch('/posts/{post}/category', [CategoryController::class, 'updatePostCategory'])->name('posts.update-category');
+
+Route::get('/posts', [PostController::class, 'index'])->name('posts.manage');
+Route::post('/posts', [PostController::class, 'store'])->name('posts.store');
+Route::patch('/posts/{post}', [PostController::class, 'update'])->name('posts.update');
+Route::delete('/posts/{post}', [PostController::class, 'destroy'])->name('posts.destroy');
+
 Route::get('post/{postId}', [PostController::class, 'show'])->name('post.show');
 Route::view('/about', 'layouts.about')->name('about');
 Route::view('/contact', 'layouts.contact')->name('contact');
