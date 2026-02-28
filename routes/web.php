caa58222c849cd2395f3a080489aec6bd18e9f9a
zwan the marvelous <azwangenshin@gmail.com>
2026-03-01 03:04:33 +0800

    added route for sigin blade

--- routes/web.php
+++ routes/web.php
@@ -5,13 +5,16 @@ use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Facades\DB;
 use App\Http\Controllers\PostController;
+use App\Http\Controllers\SigninController;
  
 
-Route::get('/', [HomeController::class, 'index'])->name('home');
+Route::get('/', [HomeController::class, 'index'])->middleware('auth')->name('home');
 Route::get('post/{postId}', [PostController::class, 'show'])->name('post.show');
 Route::view('/about', 'layouts.about')->name('about');
 Route::view('/contact', 'layouts.contact')->name('contact');
 Route::view('article', 'layouts.article')->name('article');
+Route::get('signin', [SigninController::class, 'show'])->name('signin');
+Route::post('signin', [SigninController::class, 'store'])->name('signin.store');
 
 Route::get('/dashboard', function () {
     return view('dashboard');
